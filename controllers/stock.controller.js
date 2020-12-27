279877d0dd3030fea06aa91ee3dc12ac40921cf9
Guy Yehoshua <guyyy20022@gmail.com>
2020-12-27 19:13:31 +0200

    asdfdsaas

--- controllers/stock.controller.js
+++ controllers/stock.controller.js
@@ -78,9 +78,7 @@ exports.getImage = (req, res) => {
             res.status(404).send({message: "Couldn't find Stock with thid id : " + id});
         }
         else {
-            for(let i = 0; i <= data.image_name; i++) {
-                res.status(200).sendFile(path.join(__dirname + "/../uploads/", "/" + data.image_name[i]));
-            }
+            res.status(200).sendFile(path.join(__dirname + "/../uploads/", "/" + imgName));
         }
     })
 }
